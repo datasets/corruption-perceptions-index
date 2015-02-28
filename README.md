f4f8ff989bf899af13effb757bb9ca7cb84dbde7
yannael <yannael@gmail.com>
2015-02-27 20:17:41 -0600

    Modified README.md, clean code, fix bug with commas in CSV

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
-**Transparency International (TI)** defines corruption as the abuse of entrusted power for private gain. This definition encompasses corrupt practices in both the public and private sectors. The Corruption Perceptions Index (CPI) ranks countries according to the perception of corruption in the public sector. The CPI is an aggregate indicator that combines different sources of information about corruption, making it possible to compare countries.
+Corruption Perceptions Index (CPI) as issued yearly by Transparency International. 
 
-## Source
+##Data
 
-The data is sourced from Transparency International (specifically [this dataset][current])
+The data is sourced from [Transparency International](http://www.transparency.org/research/cpi/overview). 
 
-Note that the CPI has changed in scale over the years:
-* 2001-2009
-* 2010-2012
-* 2013-2014
+> The Corruption Perceptions Index (CPI) ranks countries/territories in terms of the degree to which corruption is perceived to exist among public officials and politicians. It draws on different assessments and business opinion surveys carried out by independent and reputable institutions. It captures information about the administrative and political aspects of corruption. Broadly speaking, the surveys and assessments used to compile the index include questions relating to bribery of public officials, kickbacks in public procurement, embezzlement of public funds, and questions that probe the strength and effectiveness of public sector anti-corruption efforts. 
+
+
+Note: As part of the update to the methodology used to calculate the CPI in 2012, a new scale of 0-100 was established. Using this scale, CPI scores can be compared from one year to the next. Because of the update in the methodology, however, CPI scores before 2012 are not comparable over time.
+
+More information about the index and methodology can be found [here](http://www.transparency.org/cpi2014/in_detail)
+
+### data/*.csv
+
+One CSV file per year, fromm 1998 to 2014.
+
+## Preparation
+
+This package includes an R script (scripts/script.R) to download files from the Transparency International website, and to convert them in CSV format.
+
+## License
+
+This Data Package is licensed by its maintainers under the [Public Domain Dedication and License (PDDL)](http://opendatacommons.org/licenses/pddl/1.0/).
+
+Note that underlying rights, terms and conditions in the data from the source are unclear and may exists.
+
+
+
--- scripts/script.R
+++ scripts/script.R
@@ -1,47 +1,137 @@
 require("xlsx")
-require("dpmr")
+require("rvest")
 
-download.file("http://data.okfn.org/data/core/country-codes/r/country-codes.csv","country_codes.csv")
-country_code<-read.csv("country_codes.csv")
+dir.create("data")
 
-url<-"http://files.transparency.org/content/download/1900/12610/file/CPI2014_ResultsSpreadsheet.xlsx"
-if(url.exists(url)) {
-  download.file(url,"CPI.xlsx")
-  all_data<-read.xlsx("CPI.xlsx", sheetIndex = 1)
-  subset_data<-all_data[-c(1,2),c(2,3)]
-  colnames(subset_data)<-c("Country/Territory","CPI 2014")
+#There is no consistent storage of files containing CPI data
+#We need to reference them individually, for each year
+#And to process them individually
+#Hence the code below is somewhat repetitive, though slightly different
+
+url2014<-"http://files.transparency.org/content/download/1857/12438/file/CPI2014_DataBundle.zip"
+url2013<-"http://files.transparency.org/content/download/702/3015/file/CPI2013_DataBundle.zip"
+url2012<-"http://files.transparency.org/content/download/533/2213/file/2012_CPI_DataPackage.zip"
+url2011<-"http://files.transparency.org/content/download/313/1264/file/CPI2011_DataPackage.zip"
+url2010<-"http://files.transparency.org/content/download/426/1752/CPI+2010+results_pls_standardized_data.xls"
+#Before year 2009, HTML pages pages to be scraped
+#The base url is http://www.transparency.org/research/cpi/cpi_XXXX
+#where XXXX stands for the year (between 1998 and 2009)
+
+#2014
+tryCatch({
+    message("Downloading and extracting CPI data 2014")
+  download.file(url2014,"CPI2014_DataBundle.zip")
+  unzip("CPI2014_DataBundle.zip")
+  all_data<-read.xlsx("CPI2014_DataBundle/CPI 2014_Regional with data source scores_final.xlsx", sheetIndex = 1,stringsAsFactors=F)
+  subset_data2014<-all_data[-c(1,2),c(2,6)]
+  subset_data2014[,1]<-sub(",","",subset_data2014[,1])
+  colnames(subset_data2014)<-c("Country/Territory","CPI")
+  write.csv(subset_data2014,"data/CPI2014.csv",quote=F,row.names=F)
+  message("CPI data 2014 succesfully downloaded and saved in data/CPI2014.csv")
+}, warning = function(cond) {
+    message("CPI data 2014 caused a warning:")
+    message(cond)
+}, error = function(cond) {
+    message("CPI data 2014 caused an error:")
+    message(cond)
+})
+#2013
+tryCatch({
+    message("Downloading and extracting CPI data 2013")
+    download.file(url2013,"CPI2013_DataBundle.zip")
+  unzip("CPI2013_DataBundle.zip")
+  all_data<-read.xlsx("CPI2013_DataBundle/CPI2013_GLOBAL_WithDataSourceScores.xls", sheetIndex = 1, stringsAsFactors=F)
+  subset_data2013<-all_data[-1,c(2,7)]
+  subset_data2013[,1]<-sub(",","",subset_data2013[,1])
+  colnames(subset_data2013)<-c("Country/Territory","CPI")
+  write.csv(subset_data2013,"data/CPI2013.csv",quote=F,row.names=F)
+  message("CPI data 2013 succesfully downloaded and saved in data/CPI2013.csv")
+}, warning = function(cond) {
+    message("CPI data 2013 caused a warning:")
+    message(cond)
+}, error = function(cond) {
+    message("CPI data 2013 caused an error:")
+    message(cond)
+})
+#2012
+tryCatch({
+    message("Downloading and extracting CPI data 2012")
+    download.file(url2012,"2012_CPI_DataPackage.zip")
+  unzip("2012_CPI_DataPackage.zip")
+  all_data<-read.xlsx("2012_CPI_DataPackage/CPI2012_Results.xls", sheetIndex = 1, stringsAsFactors=F)
+  subset_data2012<-all_data[-1,c(2,4)]
+  subset_data2012[,1]<-sub(",","",subset_data2012[,1])
+  colnames(subset_data2012)<-c("Country/Territory","CPI")
+  write.csv(subset_data2012,"data/CPI2012.csv",quote=F,row.names=F)
+  message("CPI data 2012 succesfully downloaded and saved in data/CPI2012.csv")
+}, warning = function(cond) {
+    message("CPI data 2012 caused a warning:")
+    message(cond)
+}, error = function(cond) {
+    message("CPI data 2012 caused an error:")
+    message(cond)
+})
+#2011
+tryCatch({
+    message("Downloading and extracting CPI data 2011")
+    download.file(url2011,"CPI2011_DataPackage.zip")
+  unzip("CPI2011_DataPackage.zip")
+  #Below, if sheetIndex data is used, wrong CPI data are returned. Use of sheetName works. Not sure why.
+  all_data<-read.xlsx2("CPI2011_DataPackage/CPI2011_Results.xls", sheetName = "Global", stringsAsFactors=F)
+  subset_data2011<-all_data[-1,c(2,3)]
+  subset_data2011[,1]<-sub(",","",subset_data2011[,1])
+  colnames(subset_data2011)<-c("Country/Territory","CPI")
+  write.csv(subset_data2011,"data/CPI2011.csv",quote=F,row.names=F)
+  message("CPI data 2011 succesfully downloaded and saved in data/CPI2011.csv")
+}, warning = function(cond) {
+    message("CPI data 2011 caused a warning:")
+    message(cond)
+}, error = function(cond) {
+    message("CPI data 2011 caused an error:")
+    message(cond)
+})
+#2010
+tryCatch({
+    message("Downloading and extracting CPI data 2010")
+    download.file(url2010,"CPI2010.xls")
+  all_data<-read.xlsx("CPI2010.xls", sheetIndex = 1, stringsAsFactors=F)
+  subset_data2010<-all_data[-c(1:3),c(2,3)]
+  colnames(subset_data2010)<-c("Country/Territory","CPI")
+  subset_data2010[,1]<-sub(",","",subset_data2010[,1])
+  write.csv(subset_data2010,"data/CPI2010.csv",quote=F,row.names=F)
+  message("CPI data 2010 succesfully downloaded and saved in data/CPI2010.csv")
+}, warning = function(cond) {
+    message("CPI data 2010 caused a warning:")
+    message(cond)
+}, error = function(cond) {
+    message("CPI data 2010 caused an error:")
+    message(cond)
+})
+#For 1998:2009 
+#The format of each page is stable enough for a function
+#to extract the table from the page url
+getCPITableFromWebPage<-function(url) {
+  webpage<-html(url)
+  #Trick there as countries/CPI may be shifted by one column
+  col1<-html_text(html_nodes(webpage, "td:nth-child(1)"))
+  col2<-html_text(html_nodes(webpage, "td:nth-child(2)"))
+  col3<-html_text(html_nodes(webpage, "td:nth-child(3)"))
+  #If what we get in second column is not numeric, columns must be shifted
+  countries<-col1[is.na(as.numeric(col1))]
+  to_shift<-which(!is.na(as.numeric(col2)))
+  col3[to_shift]<-col2[to_shift]
+  col2[to_shift]<-countries
+  subset_data<-cbind(col2,col3)
+  colnames(subset_data)<-c("Country/Territory","CPI")
+  subset_data
 }
 
-meta<-list(
-    name="corruption-perceptions-index",
-    title="Corruption Perceptions Index (CPI)",
-    description="Corruption Perceptions Index (CPI), as reported by Transparency International, since 2001.",
-    license=list(
-        "type"= "odc-pddl",
-        "url"= "http://opendatacommons.org/licenses/pddl/"),
-    version="1.0-beta.10",
-    last_updated="2015-02-21",
-    sources=list(
-        "name"= "World Bank and OECD",
-        "web"="http://www.transparency.org/cpi2014/"
-      ),
-    keywords=list("CPI","Transparency International", "Corruption Perceptions Index")
-  )
-
-datapackage_init(subset_data,"corruption-perceptions-index",meta,"script.R")
-
-
-#http://www.transparency.org/cpi2014/results#myAnchor1
-
-#http://files.transparency.org/content/download/1900/12610/file/CPI2014_ResultsSpreadsheet.xlsx
-
-http://files.transparency.org/content/download/1857/12438/file/CPI2014_DataBundle.zip
-http://files.transparency.org/content/download/702/3015/file/CPI2013_DataBundle.zip
-http://files.transparency.org/content/download/533/2213/file/2012_CPI_DataPackage.zip
-http://files.transparency.org/content/download/313/1264/file/CPI2011_DataPackage.zip
-http://files.transparency.org/content/download/426/1752/CPI+2010+results_pls_standardized_data.xls
-http://www.transparency.org/files/content/tool/2009_CPI_SourcesByCountry.xls
-http://www.transparency.org/files/content/tool/2008_CPI_SourcesByCountry.xls
-
-
-
+for (year in 1998:2009) {
+    message(paste0("Downloading and extracting CPI data ",year))
+        url<-paste("http://www.transparency.org/research/cpi/cpi_",year,sep="")
+  filename<-paste0("data/CPI",year,".csv")
+  data<-getCPITableFromWebPage(url)
+  data[,1]<-sub(",","",data[,1])
+  write.csv(data,filename,quote=c(1),row.names=F)
+  message(paste0("CPI data ",year," succesfully downloaded and saved in data/CPI",year,".csv"))
+}
